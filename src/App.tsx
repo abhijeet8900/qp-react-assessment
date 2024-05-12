7e59ea65020bb9beaa987f7f13f771f1ead98e71
abhijeet8900 <abhijeet8900@gmail.com>
2024-05-12 12:16:52 +0530

    - Store todos in localstorage

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,43 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 import TodoList from "./components/todo-list";
 import { Item } from "./types/item";
 
 function App() {
-  const [items, setItems] = useState<Item[]>([]);
+  const [todos, setTodos] = useState<Item[]>([]);
+
+  // Load TODOs from local storage on app startup
+  useEffect(() => {
+    const storedTodos = localStorage.getItem("todos");
+    if (storedTodos) {
+      const parsedTodos = JSON.parse(storedTodos);
+      setTodos(parsedTodos);
+    }
+  }, []);
+
+  // Update local storage whenever TODOs change
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos]);
 
   const addNewItem = (item: Item) => {
-    const updateItems = [item, ...items];
-    setItems(updateItems);
+    const updateItems = [item, ...todos];
+    setTodos(updateItems);
   };
 
   const updateItemStatus = (item: Item) => {
-    const updatedItems = items.map((todoItem) => {
+    const updatedItems = todos.map((todoItem) => {
       if (todoItem.id === item.id) {
         return { ...todoItem, checked: !todoItem.checked };
       }
       return todoItem;
     });
-    setItems(updatedItems);
+    setTodos(updatedItems);
   };
   return (
     <div className="App bg-gray-100 h-screen w-screen">
       <TodoList
-        items={items}
+        items={todos}
         addItem={addNewItem}
         changeItemStatus={updateItemStatus}
       />
